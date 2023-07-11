6c8c57a2dce0111e9b09a04abbfaa2bf9e1d4664
坪内勇人 <saikyousan15@gmail.com>
2023-07-11 12:56:58 +0900

    Vue practice

--- Vue3-practice/vue3-practice2/src/App6/App61.vue
+++ Vue3-practice/vue3-practice2/src/App6/App61.vue
@@ -2,7 +2,9 @@
 import {ref} from 'vue'
 const myName = ref('ゲスト')
 const gender = ref('不明')
-const agree = ref(true)
+const agree = ref('同意しません。')
+const language = ref(['日本語', '英語'])
+const subject = ref(['国語', '英語'])
 </script>
 
 <template>
@@ -30,8 +32,30 @@ const agree = ref(true)
         <div>
             <p>上記の内容に同意しますか？</p>
             <label for="agree">同意する：</label>
-            <input type="checkbox" id="agree" v-model="agree" true-value = "同意します。" fale-value = "同意しません。"/>
+            <input type="checkbox" id="agree" v-model="agree" true-value = "同意します。" false-value = "同意しません。"/>
             <p>解答：{{ agree }}</p>
+        </div>
+        <div>
+            <p>話せる言語は？</p>
+            <label for="japanese">日本語</label>
+            <input type="checkbox" id = "japanese" value = "日本語" v-model="language"/>
+            <label for="english">英語</label>
+            <input type="checkbox" id = "english" value = "英語" v-model="language"/>
+            <label for="german">ドイツ語</label>
+            <input type="checkbox" id = "german" value = "ドイツ語" v-model="language"/>
+            <p>解答：{{ language }}</p>
+        </div>
+        <div>
+            <p>好きな教科を選んでください！</p>
+            <select v-model="subject" multiple size = "5">
+                <option value="">好きな教科は？</option>
+                <option>国語</option>
+                <option>英語</option>
+                <option>数学</option>
+                <option>理科</option>
+                <option>社会</option>
+            </select>
+            <p>好きな教科：{{ subject }}</p>
         </div>
     </form>
 </template>
